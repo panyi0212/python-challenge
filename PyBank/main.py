3b125ce63a3d601533ed3461d0cd673856c2f8a4
panyi0212 <123052286+panyi0212@users.noreply.github.com>
2023-06-25 22:20:49 -0700

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,69 +1,81 @@
 import csv
 import os
-import pandas as pd
-import numpy as np
 
 # import the location of the file
 filepath=os.path.join("Resources","budget_data.csv")
 
-# use pandas to read the csv file
-budget_data = pd.read_csv(filepath, encoding="ISO-8859-1")
+total_months = 0
+total_profit = 0
+profit_change =0
+value = 0
+greatest_increase = 0
+greatest_decrease = 0
+months_change = 0
+dates = []
+profit = []
 
-# Count the number of total months
-month = len(budget_data)
-
-#Sum the total profit/loss and format
-total = budget_data["Profit/Losses"].sum()
-total = '${:.0f}'.format(total)
-
-#Use Diff()to calculate the average change and use mean to calculate the average number
-average = budget_data["Profit/Losses"].diff().mean()
-average = '${:.2f}'.format(average)
-
-
-# Add colume to budget_data for the difference
-diff=budget_data["Profit/Losses"].diff()
-budget_data["Diff"] = diff
-
-#Find the max increase
-increase = diff.max()
-
-#Locate the month with max increase
-max_increase = budget_data.loc[(budget_data["Diff"]==increase),"Date"].item()
-print(max_increase)
-
-#Find the row info of the max decrease
-decrease = diff.min()
-
-#Locate the month with max decrease
-max_decrease = budget_data.loc[(budget_data["Diff"]==decrease),"Date"].item()
-
-#change the format
-increase='${:.0f}'.format(increase)
-decrease='${:.0f}'.format(decrease)
+# define variables
+def print_percentages(budget_data):
+    month_day = str(budget_data[0])
+    profit_losses = int(budget_data[1])
 
 
+# open and read csv file
+with open(filepath, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        header = next(csv_reader)
+
+        row_one = next(csv_reader)
+        total_months = total_months + 1
+        total_profit = int(total_profit) + int(row_one[1])
+        value = int(row_one[1])
+        
+# run loop through csv
+        for row in csv_reader:
+            dates.append(row[0])
+
+# keep track of change
+            profit_change = int(row[1])-value
+            profit.append(profit_change)
+            value = int(row[1])
+
+# The total number of months included in the dataset
+            total_months = total_months + 1
+
+# The net total amount of "Profit/Losses" over the entire period
+            total_profit = int(total_profit) + int(row[1])
+
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+            average_change = round(sum(profit)/len(profit),2)
+
+# The greatest increase in profits (date and amount) over the entire period
+            greatest_increase = max(profit)
+            greatest_value = profit.index(greatest_increase)
+            greatest_date = dates[greatest_value]
+
+# The greatest decrease in profits (date and amount) over the entire period
+            greatest_decrease = min(profit)
+            lowest_value = profit.index(greatest_decrease)
+            lowest_date = dates[lowest_value]
+
+
+
+# print in terminal
 print("Financial Analysis")
-print("----------------------------")
-print(f'Total Months: {month}')
-print(f'Total: {total}')
-print(f'Average Change: {average}')
-print(f'Greatest Increase in Profits: {max_increase} ({increase})')
-print(f'Greatest Decrease in Profits: {max_decrease} ({decrease})')
+print('----------------------------------------')
+print (f"Total Months: {total_months}")
+print (f"Total: ${total_profit}")
+print (f"Average Change: ${average_change}")
+print (f"Greatest Increase in Profits: {greatest_date} (${greatest_increase})")
+print (f"Greatest Decrease in Profits: {lowest_date} (${greatest_decrease})")
 
-# create path for writing to a csv
-
-output_path = os.path.join("Analysis", "analysis.csv")
-
-# open the csv file
-with open(output_path, 'w') as csvfile:
-
-    csvwriter = csv.writer(csvfile) 
-    csvwriter.writerow(["Financial Analysis"]) 
-    csvwriter.writerow(["-------------------------"])
-    csvwriter.writerow([f'Total Months: {month}'])
-    csvwriter.writerow([f'Total: {total}'])
-    csvwriter.writerow([f'Average Change: {average}'])
-    csvwriter.writerow([f'Greatest Increase in Profits: {max_increase} ({increase})'])
-    csvwriter.writerow([f'Greatest Decrease in Profits: {max_decrease} ({decrease})'])
-
+# create text file with results
+with open("PyBank_result.txt", "w") as file:
+    file.write("\nFinancial Analysis")
+    file.write('\n----------------------------------------')
+    file.write(f"\nTotal Months: {total_months}")
+    file.write(f"\nTotal: ${total_profit}")
+    file.write(f"\nAverage Change: ${average_change}")
+    file.write(f"\nGreatest Increase in Profits: {greatest_date} (${greatest_increase})")
+    file.write(f"\nGreatest Decrease in Profits: {lowest_date} (${greatest_decrease})")
+    file.close()
